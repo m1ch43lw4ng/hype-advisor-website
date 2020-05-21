4590a6fa7bfda4e314b1437ec7d4e28b570b83ad
Addison Chan <addison95132@gmail.com>
2020-05-20 21:19:24 -0700

    moved css to directory

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
-import "./layout.css"
+import "../css/layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
