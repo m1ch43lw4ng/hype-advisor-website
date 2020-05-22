339268ce39d5555c64905664324e86b77095014a
Addison Chan <addison95132@gmail.com>
2020-05-22 00:03:43 -0700

    finished interest form

--- src/pages/interest_form.js
+++ src/pages/interest_form.js
@@ -16,35 +16,26 @@ const IndexPage = () => (
           10281 Vicksburg Drive <br/>
           Cupertino, CA 95132 <br/><br/>
 
-          +123 456 789<br/><br/>
+          +408 887 4558<br/><br/>
 
           team@thehypeadvisor.com
         </p></div>
-        <div id="follow-us"><h4>Follow Us</h4></div>
+        <div id="Interest_Form-follow-us"><h4>Follow Us</h4></div>
         {/* <div class="social-media">#TODO: reuse footer component</div> */}
       </div>
       <form id="right-bar">
         <h1 id="interest-form">Interest Form</h1>
-        <section>
-          <p>
-            <label for="name">
-              <span>Name:</span>
-            </label>
-            <input type="text" id="name" name="name"/>
-          </p>
-          <p>
-            <label for="email">
-              <span>Email:</span>
-            </label>
-            <input type="email" id="email" name="email"/>
-          </p>
-          <p>
-            <label for="message">
-              <span>Message:</span>
-            </label>
-            <input type="textarea" id="message" name="message"/>
-          </p>
+        <section id="Interest_Form-data">
+          <h4><label for="name"> <span>Name:</span> </label></h4>
+          <p><input type="text" id="name" placeholder="Your Name" name="name"/></p>
+
+          <h4><label for="email"> <span>Email:</span> </label></h4>
+          <p><input type="email" id="email" placeholder="Your Email" name="email"/></p>
+
+          <h4><label for="message"> <span>Message:</span> </label></h4>
+          <p><textarea type="textarea" id="message" placeholder="Your Message" name="message"/></p>
         </section>
+        <div id="Interest_Form-send"><p>Send</p></div>
       </form>
     </div>
   </Layout>
